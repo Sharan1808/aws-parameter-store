bfe2318aea4658295da23ebaf85b6f50f14b00f2
Sharan <sharanpyata1808@gmail.com>
2024-01-15 20:30:52 -0600

    Terraform module updates

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket = "d76s-terraform-state"
+    key = "parameter-store/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+variable "parameters" {
+  default = [
+    { name = "test", type = "string", value = "test"},
+    { name = "test1", type = "string", value = "test1"}
+  ]
+}
+
+resource "aws_ssm_parameter" "params" {
+  count = length(var.parameters)
+  name = var.parameters[count.index].name
+  type = var.parameters[count.index].type
+  value = var.parameters[count.index].value
+}
+
