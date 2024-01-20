108b96a0400467ae92186412921a910c6402c770
Sharan <sharanpyata1808@gmail.com>
2024-01-20 16:13:48 -0600

    Terraform code

--- vars.tf
+++ vars.tf
@@ -2,6 +2,7 @@ variable "parameters" {
   default = [
     { name = "ses.username", type = "String", value = "AKIAUSOYUYMYQY2CI6XQ" },
     { name = "dev.rds.username", type = "String", value = "admin1" },
+    { name = "dev.backend.DB_HOST", type = "String", value = "dev-mysql-rds.cbcy0m0iwk13.us-east-1.rds.amazonaws.com" },
 
 
     ##passwords
