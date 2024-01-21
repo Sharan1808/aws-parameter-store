d6e056d6dc025070b9a30951d8fc9de9bc9a7d58
Sharan <sharanpyata1808@gmail.com>
2024-01-21 17:20:09 -0600

    Terraform code

--- vars.tf
+++ vars.tf
@@ -2,7 +2,7 @@ variable "parameters" {
   default = [
     { name = "ses.username", type = "String", value = "AKIAUSOYUYMYQY2CI6XQ" },
     { name = "dev.backend.DB_HOST", type = "String", value = "dev-mysql-rds.cbcy0m0iwk13.us-east-1.rds.amazonaws.com" },
-    { name = "dev.frontend.BACKEND_ENDPOINT", type = "String", value = "http://internal-dev-backend-538371852.us-east-1.elb.amazonaws.com/" },
+    { name = "dev.frontend.BACKEND_ENDPOINT", type = "String", value = "http://backend-dev.spdevops18.online/" },
 
     ##RDS
     { name = "dev.rds.endpoint", type = "String", value = "dev-mysql-rds.cbcy0m0iwk13.us-east-1.rds.amazonaws.com" },
