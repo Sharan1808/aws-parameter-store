17abc4e9972d1470735b7dc58ec1c24727102f19
Sharan <sharanpyata1808@gmail.com>
2024-02-20 17:11:18 -0600

    Terraform code

--- vars.tf
+++ vars.tf
@@ -3,6 +3,7 @@ variable "parameters" {
     { name = "ses.username", type = "String", value = "AKIAUSOYUYMYQY2CI6XQ" },
     { name = "dev.backend.DB_HOST", type = "String", value = "dev-mysql-rds.cbcy0m0iwk13.us-east-1.rds.amazonaws.com" },
     { name = "dev.frontend.BACKEND_ENDPOINT", type = "String", value = "http://backend-dev.spdevops18.online/" },
+    { name = "ssh.username", type = "String", value = "root" },
 
     ##RDS
     { name = "dev.rds.endpoint", type = "String", value = "dev-mysql-rds.cbcy0m0iwk13.us-east-1.rds.amazonaws.com" },
@@ -12,5 +13,6 @@ variable "parameters" {
     { name = "dev.rds.password", type = "SecureString", value = "ExpenseApp1234" },
     { name = "sonar.token", type = "SecureString", value = "squ_2b5822337c5a683ab4164328e0c5fb78696e09d5" },
     { name = "artifactory.password", type = "SecureString", value = "Admin123" },
+    { name = "ssh.password", type = "SecureString", value = "DevOps321" },
   ]
 }
