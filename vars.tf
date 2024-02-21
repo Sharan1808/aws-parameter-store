8c98aeac27544f6aefb17efac4a24f24bbbd869d
Sharan <sharanpyata1808@gmail.com>
2024-02-21 15:08:39 -0600

    Terraform code

--- vars.tf
+++ vars.tf
@@ -6,6 +6,7 @@ variable "parameters" {
     "ssh.username" = {name = "ssh.username", type = "String", value = "root"},
     "artifactory.username" = {name = "artifactory.username", type = "String", value = "admin"},
     "dev.backend.app_version" = {name = "dev.backend.app_version", type = "String", value = "1.0.0"},
+    "dev.frontend.app_version" = {name = "dev.frontend.app_version", type = "String", value = "1.0.0"},
 
 
     ##RDS
@@ -19,6 +20,6 @@ variable "parameters" {
     "dev.rds.password" = {name = "dev.rds.password", type = "SecureString", value = "ExpenseApp1234"},
     "sonar.token" = {name = "sonar.token", type = "SecureString", value = "squ_2b5822337c5a683ab4164328e0c5fb78696e09d5"},
     "artifactory.password" = {name = "artifactory.password", type = "SecureString", value = "Admin123"},
-    "ssh.password" = {name = "ssh.password", type = "SecureString", value = "DevOps321"},
+    "ses.password" = {name = "ssh.password", type = "SecureString", value = "DevOps321"},
   }
 }
