9195d36b50d8fce1dc9abd6b21e3935a53d57c30
Sharan <sharanpyata1808@gmail.com>
2024-02-21 14:40:34 -0600

    Terraform code

--- vars.tf
+++ vars.tf
@@ -1,20 +1,24 @@
 variable "parameters" {
-  default = [
-    { name = "ses.username", type = "String", value = "AKIAUSOYUYMYQY2CI6XQ" },
-    { name = "dev.backend.DB_HOST", type = "String", value = "dev-mysql-rds.cbcy0m0iwk13.us-east-1.rds.amazonaws.com" },
-    { name = "dev.frontend.BACKEND_ENDPOINT", type = "String", value = "http://backend-dev.spdevops18.online/" },
-    { name = "ssh.username", type = "String", value = "root" },
-    { name = "artifactory.username", type = "String", value = "admin" },
-    { name = "dev.backend.app_version", type = "String", value = "1.0.0" },
+  default = {
+    "ses.username" = {name = "ses.username", type = "String", value = "AKIAUSOYUYMYQY2CI6XQ" } ,
+    "dev.backend.DB_HOST" = {name = "dev.backend.DB_HOST", type = "String", value = "dev-mysql-rds.cbcy0m0iwk13.us-east-1.rds.amazonaws.com"},
+    "dev.frontend.BACKEND_ENDPOINT" = {name = "dev.frontend.BACKEND_ENDPOINT", type = "String", value = "http://backend-dev.spdevops18.online/"},
+    "ssh.username" = {name = "ssh.username", type = "String", value = "root"},
+    "artifactory.username" = {name = "artifactory.username", type = "String", value = "admin"},
+    "dev.backend.app_version" = {name = "dev.backend.app_version", type = "String", value = "1.0.0"},
+
 
     ##RDS
-    { name = "dev.rds.endpoint", type = "String", value = "dev-mysql-rds.cbcy0m0iwk13.us-east-1.rds.amazonaws.com" },
-    { name = "dev.rds.username", type = "String", value = "admin1" },
+    "dev.rds.endpoint" = {name = "dev.rds.endpoint", type = "String", value = "dev-mysql-rds.cbcy0m0iwk13.us-east-1.rds.amazonaws.com"},
+    "dev.rds.username" = {name = "dev.rds.username", type = "String", value = "admin1"},
+
+
+
 
     ##passwords
-    { name = "dev.rds.password", type = "SecureString", value = "ExpenseApp1234" },
-    { name = "sonar.token", type = "SecureString", value = "squ_2b5822337c5a683ab4164328e0c5fb78696e09d5" },
-    { name = "artifactory.password", type = "SecureString", value = "Admin123" },
-    { name = "ssh.password", type = "SecureString", value = "DevOps321" },
-  ]
+    "dev.rds.password" = {name = "dev.rds.password", type = "SecureString", value = "ExpenseApp1234"},
+    "sonar.token" = {name = "sonar.token", type = "SecureString", value = "squ_2b5822337c5a683ab4164328e0c5fb78696e09d5"},
+    "artifactory.password" = {name = "artifactory.password", type = "SecureString", value = "Admin123"},
+    "ssh.password" = {name = "ssh.password", type = "SecureString", value = "DevOps321"},
+  }
 }
